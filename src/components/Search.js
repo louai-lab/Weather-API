7af26764b60d423f3068e1af138190ce2d8c7d6e
louai <louaibaghdadi27@gmail.com>
2023-09-12 23:51:25 +0300

    Weather API: Breaking the HTML in App.js into React components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,31 @@
 import React from "react";
-
-import clear from "../img/weather-icons/clear.svg";
 import "./Search.css";
-class Search extends React.Component {
-  state = {
-    input: ""
-  };
 
-  render() {
-    return (
-      <div>
-        {this.state.input}
-        <input
-          type="text"
-          id="input-name"
-          onChange={event => {
-            this.setState({ input: event.target.value });
-          }}
-        />
-        <button
-          onClick={event => {
-            this.props.handleInput(this.state.input);
-          }}
-        >
-          Say Hello
-        </button>
-      </div>
-    );
-  }
+function Search(){
+  return (
+    <div className="search">
+      <input type="text" placeholder="Type in city name"/>
+      <button className="btn">Find Weather</button>
+    </div>
+  );
 }
 export default Search;
+
+
+
+
+{/* {this.state.input}
+      <input
+        type="text"
+        id="input-name"
+        onChange={event => {
+          this.setState({ input: event.target.value });
+        }}
+      />
+      <button
+        onClick={event => {
+          this.props.handleInput(this.state.input);
+        }}
+      >
+        Say Hello
+      </button> */}
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from "react";
+import img from "../img/weather-icons/clear.svg"
 
-export default class WeatherItem extends Component {
-  render() {
-    return <div>Heello</div>;
-  }
+function CurrentWeather(){
+  return(
+    <div className="content">
+      <img src={img}></img>
+      <h2>Description</h2>
+      <p className="firstPara"><span className="temp">Temperature </span>10 ْ to 15 ْC</p>
+      <p><span className="a">Humidity </span>78% <span className="a">Pressure </span>1008.48</p>
+    </div>
+  )
 }
-export class SayHello extends Component {
-  render() {
-    return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.name}
-      </div>
-    );
-  }
+
+
+function WeatherItem(){
+  return(
+    <div>
+      
+    </div>
+  )
 }
+
+
+export {CurrentWeather,WeatherItem};
